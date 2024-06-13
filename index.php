13b12c01428d5683db0d8e7c73f1731cc3ded637
surfercris <surfercris1972@gmail.com>
2024-06-13 18:06:35 +0100

    Update index.php

--- index.php
+++ index.php
@@ -4,23 +4,6 @@
 <?php include('dbfilmes.sql'); ?>
 
 
-<?php
-// db_connection.php
-
-$servername = "127.0.0.1";
-$username = "root"; // Altere conforme necessário
-$password = ""; // Altere conforme necessário
-$dbname = "dbfilmes";
-
-// Criar conexão
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-// Verificar conexão
-if ($conn->connect_error) {
-    die("Falha na conexão: " . $conn->connect_error);
-}
-?>
-
 
     <div class="box1">
     <button class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">Adicionar</button>
