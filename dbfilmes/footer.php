38792348da7cb47dbd45371747a13ca7287b344d
surfercris <surfercris1972@gmail.com>
2024-06-13 16:36:03 +0100

    Delete dbfilmes directory

--- dbfilmes/footer.php
+++
@@ -1,9 +0,0 @@
-</div>
-     <!-- JS, Popper.js, and jQuery -->
-<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH01sSSs5nCTpuj/zy4C+0GpamoFVy38MVBNE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity=" sha384-9/reFTGAW83EW2RDu250VKaIzap3H661ZH81PoY1FhbGU+6BZp6G7niu735Sk71N" crossorigin="anonymous"></script> 
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity=" sha384-B4gt1jrGC7Jh4AgTPSdUtOBvf08shuf57BaghqFfP1YxofvL8/KUEFYiJOMMV÷rV" crossorigin="anonymous"></script>
-    </body>
-    </html>
-
-   
